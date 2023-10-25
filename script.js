ab9d3e346fadabedbc843750a7cf4641a3baf0e1
ZinalPotphode <zinalvijaypotphode@gmail.com>
2023-10-25 18:02:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var form=document.getElementById("form")
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    var search=document.getElementById("search").value
+   
+    var name=search.split(' ').join('')
+    var result=document.getElementById("result")
+    result.innerHTML="";
+   try{
+    fetch("https://api.github.com/users/"+name)
+    .then((result)=>result.json())
+    .then((data)=>{console.log(data)
+    result.innerHTML=`<a target="blank" href="${data.html_url}"><div class="card">
+    <div class="uh"><div class="img"><img src="${data.avatar_url}"></div>
+    <p>Username: ${data.login}</p><p>Full name: ${data.name}</p><p>Bio: ${data.bio}</p></div><br>
+    <p>Location: ${data.location}</p>
+    <p>Number of Repositories: ${data.public_repos}</p>
+    <p>Followers: ${data.followers}</p>
+    <p>Following: ${data.following}</p></div></a>`
+})
+}
+catch(err){
+    result.innerHTML=`<div class="card"><p>No such user found</p></div>`
+}
+
+
+})
